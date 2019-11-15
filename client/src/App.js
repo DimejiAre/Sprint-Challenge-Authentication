66b4e7efa3aadb5c7d8a22bb3c01c125dfbd14ea
DimejiAre <olardi2@gmail.com>
2019-11-15 11:49:59 +0100

    add front-end application

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route} from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import Navbar from './components/Navbar';
+import Jokes from './components/Jokes';
+import './App.scss';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route
+        path='/'
+        render={props => {
+          return <Navbar {...props}/>;
+        }}
+      />
+      <Route
+        exact path='/'
+        render={props => {
+          return <LoginForm {...props}/>;
+        }}
+      />
+      <Route
+        exact path='/register'
+        render={props => {
+          return <RegisterForm {...props}/>;
+        }}
+      />
+      <Route 
+      path='/jokes'
+      render={props => {
+        return <Jokes {...props}/>
+      }}/>
     </div>
   );
 }
