22976ce1eb56dc562718866243b679233609a146
DimejiAre <olardi2@gmail.com>
2019-11-15 09:50:20 +0100

    create endpoint for register and associated db model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const db = require('./auth-model');
+const generateToken = require('../helpers/generateToken')
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  try{
+    const user = req.body;
+    const result = await db.registerUser(user)
+    const token = generateToken(result)
+    res.status(201).json({message: `welcome ${result.username}`, token: token})
+  }
+  catch (error){
+    res.status(500).json({error: 'An error occurred during registeration'})
+  }
+  
 });
 
 router.post('/login', (req, res) => {
